0c84d4294c53062f5cc35e4ef4093f56f27a652b
HHKG <972834061@qq.com>
2019-06-15 11:46:12 +0800

    导航栏切换功能

--- js/lunbo.js
+++ js/lunbo.js
@@ -7,10 +7,14 @@ $(document).ready(function() {
 	for(var i = 0; i < $('.lunbo>ul>li').length; i++) {
 		dots += '<span></span>';
 	}
+	//复制一份相同的照片放到后面
+	$('.lunbo ul').append($('.lunbo ul').html());
+	//根据图片循环出对应的点数
 	$('.dots').html(dots);
 	//鼠标移入事件，获取span本身的索引
+
 	$('.dots span').on('mouseover', function() {
-	    index = $('.dots span').index(this);
+		index = $('.dots span').index(this);
 		clearInterval(timer);
 		light(index);
 		clacluate(index);
@@ -18,10 +22,13 @@ $(document).ready(function() {
 
 	//计算ul滑动的距离
 	function clacluate(a) {
-			moveW = -a * clientW + 'px';
+		moveW = -a * clientW + 'px';
 		$('.lunbo ul').stop(true, false).animate({
 			'margin-left': moveW
 		}, 300);
+		if(a = 4) {
+			$('.lunbo ul').css('margin-left', '0px');
+		}
 	}
 	//圆点高亮
 	function light(c) {
@@ -33,7 +40,7 @@ $(document).ready(function() {
 	}
 	//自动轮播
 	function run() {
-		timer=setInterval(function() {
+		timer = setInterval(function() {
 			index += 1;
 			if(index <= 4) {
 				if(index == 4) {
@@ -46,43 +53,43 @@ $(document).ready(function() {
 		}, 2000);
 	}
 	run();
-    //显示左边按钮
-    $('.jiantou_prev').on('mouseover',function(){
-    	$('.prev').fadeIn();
-    	clearInterval(timer);
-    })
-    //隐藏左边按钮
-        $('.jiantou_prev').on('mouseleave',function(){
-    	$('.prev').fadeOut();
-    	run();
-    })
-    
-     //显示右边按钮
-    $('.jiantou_next').on('mouseover',function(){
-    	$('.next').fadeIn();
-    	clearInterval(timer);
-    })
-     //隐藏右边按钮
-    $('.jiantou_next').on('mouseleave',function(){
-    	$('.next').fadeOut();
-    	run();
-    })
-    //点击事件
-    $('.prev').click(function(){
-    	index-=1;
-    	if(index==-1){
-    		index=3;//赋值用一个等号，判断用两个等号，标准使用三个等号
-    	}
-    	light(index);
+	//显示左边按钮
+	$('.jiantou_prev').on('mouseover', function() {
+		$('.prev').fadeIn();
+		clearInterval(timer);
+	})
+	//隐藏左边按钮
+	$('.jiantou_prev').on('mouseleave', function() {
+		$('.prev').fadeOut();
+		run();
+	})
+
+	//显示右边按钮
+	$('.jiantou_next').on('mouseover', function() {
+		$('.next').fadeIn();
+		clearInterval(timer);
+	})
+	//隐藏右边按钮
+	$('.jiantou_next').on('mouseleave', function() {
+		$('.next').fadeOut();
+		run();
+	})
+	//点击事件
+	$('.prev').click(function() {
+		index -= 1;
+		if(index == -1) {
+			index = 3; //赋值用一个等号，判断用两个等号，标准使用三个等号
+		}
+		light(index);
 		clacluate(index);
-    });
-      //点击事件
-    $('.next').click(function(){
-    	index+=1;
-    	if(index==4){
-    		index=0;
-    	}
-    	light(index);
+	});
+	//点击事件
+	$('.next').click(function() {
+		index += 1;
+		if(index == 4) {
+			index = 0;
+		}
+		light(index);
 		clacluate(index);
-    })
+	})
 })
